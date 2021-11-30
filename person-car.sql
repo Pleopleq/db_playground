b269d0f251189a78a592a376a1abd13a5d3e99db
Pleopleq <felix.anducho74@gmail.com>
2021-11-29 18:47:40 -0600

    Refactor relationship between person and inventory

--- person-car.sql
+++ person-car.sql
@@ -1,13 +1,20 @@
-CREATE TABLE person (
-    id                  BIGSERIAL NOT NULL PRIMARY KEY,
-    email               VARCHAR(100),
-    inventory_id        BIGINT REFERENCES inventory (id),
-    UNIQUE(inventory_id)
-);
-
 CREATE TABLE inventory (
     id                  BIGSERIAL NOT NULL PRIMARY KEY,
+    inventory_name      VARCHAR(100)
 );
+
+CREATE TABLE person (
+    id                  BIGSERIAL NOT NULL PRIMARY KEY,
+    email               VARCHAR(100)
+);
+
+CREATE TABLE person_inventories (
+    inventory_id BIGINT,
+    person_id BIGINT,
+    PRIMARY KEY (inventory_id, person_id),
+    CONSTRAINT fk_inventory FOREIGN KEY(inventory_id) REFERENCES inventory(id),
+    CONSTRAINT fk_person FOREIGN KEY(person_id) REFERENCES person(id)
+)
 
 CREATE TABLE store (
     id                  BIGSERIAL NOT NULL PRIMARY KEY,
@@ -23,8 +30,18 @@ CREATE TABLE item (
     item_name           VARCHAR(125),
     item_description    VARCHAR(255),
     item_quantity       SMALLINT,
+    item_price          NUMERIC NOT NULL DEFAULT 0
     item_image          TEXT,
     store_id            BIGINT NOT NULL
     CONSTRAINT fk_store FOREIGN KEY(store_id) REFERENCES store(id)
 
 )
+
+INSERT INTO person (email) VALUES ('email@email.com');
+INSERT INTO person (email) VALUES ('email01@email.com');
+INSERT INTO person (email) VALUES ('email02@email.com');
+
+INSERT INTO inventory (inventory_name) VALUES ('Computer Store');
+INSERT INTO inventory (inventory_name) VALUES ('Bodega');
+
+INSERT INTO person_inventories (inventory_id, person_id) VALUES (1, 1);
